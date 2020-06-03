2affbc9d273e0aef4631caaba264c0065803ba4a
torjeikenes <torje.n.eikenes@gmail.com>
2020-06-03 16:35:13 +0200

    added bin

---
+++ bin/bspwm_workspces.sh
@@ -0,0 +1,58 @@
+#! /bin/sh
+
+MODE=$1
+s1="d"
+
+
+echo $MODE
+
+if (xrandr | grep "HDMI1 connected" > /dev/null)
+then
+    if [ "$MODE" = "d" ]
+    then
+        mons -d &
+        xinput --set-prop 9 271 1.0
+        xinput --set-prop 12 271 2.5
+    else
+        bspc monitor HDMI1 -d 1 2 3 4 5 
+        bspc monitor eDP-1 -d 6 7 8 9 10
+        xinput --set-prop 9 271 2.0
+        xinput --set-prop 12 271 4.0
+        if [ "$MODE" = "r" ]
+        then
+            mons -e right &
+        else
+            mons -e left &
+        fi
+    fi
+elif (xrandr | grep "DP-2 connected" > /dev/null)
+then
+    if [ "$MODE" == "d" ]
+    then
+        echo "hei hei"
+        mons -d &
+        xinput --set-prop 9 271 1.0
+        xinput --set-prop 12 271 2.5
+    else
+        bspc monitor eDP-1 -d 1 2 3 4 5 6 7 8 9 
+        bspc monitor DP2 -d 10
+        xinput --set-prop 9 271 2.5
+        xinput --set-prop 12 271 4.0
+        if [ "$MODE" = "r" ]
+        then
+            mons -e right &
+        else
+            mons -e left &
+        fi
+    fi
+else
+    mons -o &
+	bspc monitor eDP-1 -d 1 2 3 4 5 6 7 8 9 
+    bspc monitor DP1 -d 10
+    xinput --set-prop 9 271 1.0
+    xinput --set-prop 12 271 2.5
+fi
+
+$HOME/.config/polybar/launch.sh &
+
+
---
+++ bin/plot.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import sys, getopt
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--input", "-i", help="Set input .csv file")
+parser.add_argument("--output", "-o", help="set output file. Works with .png or .pdf")
+
+args = parser.parse_args()
+
+
+header = []
+data = []
+
+
+filename = "data.csv"
+
+if args.input:
+    filename = args.input
+
+with open(filename) as csvfile:
+    csvreader = csv.reader(csvfile)
+
+    header = next(csvreader)
+
+    for datapoint in csvreader:
+
+        values = [float(value) for value in datapoint]
+        data.append(values)
+
+print(header)
+print(data[0])
+print(data[1])
+
+time = [p[0]*1000+5 for p in data]
+ch1 = [p[1] for p in data]
+ch2 = [p[2] for p in data]
+
+plt.figure(figsize=(8,4))
+plt.plot(time,ch1, label=r'$v_0$')
+plt.plot(time,ch2, label=r'$v_1$')
+plt.xlabel('Time [$ms$]')
+plt.ylabel('Voltage [V]')
+frequency = 100
+plt.yticks(np.arange(-4, 6, 1.0))
+
+#plt.axhline(y=0, color="black")
+
+#fig, axes = plt.subplots(5, 5, figsize=(12, 10))
+output = os.path.splitext(filename)[0] + ".png"
+
+if args.output:
+    filename = args.output
+
+plt.legend(loc='upper right')
+plt.savefig(output, bbox_inches='tight',dpi=199)
+plt.show()
+

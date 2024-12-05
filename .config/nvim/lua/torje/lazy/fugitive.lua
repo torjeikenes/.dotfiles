89ec64e9326460d845fd7dd1a6db1f1c550e80ed
TorjeNEikenes <tne@datarespons.no>
2024-12-05 16:07:49 +0100

    Add merge conflict keybinds

--- .config/nvim/lua/torje/lazy/fugitive.lua
+++ .config/nvim/lua/torje/lazy/fugitive.lua
@@ -3,4 +3,7 @@ return {
 	vim.keymap.set("n", "<leader>gs", vim.cmd.Git),
 	vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>", { desc = "accept the left git merge change" }),
 	vim.keymap.set("n", "gj", "<cmd>diffget //3<CR>", { desc = "accept the right git merge change" }),
+
+	vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit!<CR>", { desc = "[G]it vertical [D]iff" }),
+	vim.keymap.set("n", "<leader>gh", "/HEAD<CR>", { desc = "[G]oto next [H]EAD" }),
 }
